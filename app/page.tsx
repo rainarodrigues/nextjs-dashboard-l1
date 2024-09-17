f54340510b44f95f06f6158ba7966f915ca581b3
Raina Rodrigues de Lima <raina.rodrigues@iplanfor.fortaleza.ce.gov.br>
2024-09-17 11:51:35 -0300

    configurando o banco de dados

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import AcmeLogo from '@/app/ui/acme-logo';
 
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
-import { Lusitana } from 'next/font/google';
-import Image from 'next/image';
+//import styles from '@/app/ui/home.module.css'
+//import { Lusitana } from 'next/font/google';
+//import Image from 'next/image';
 
 export default function Page() {
   return (
